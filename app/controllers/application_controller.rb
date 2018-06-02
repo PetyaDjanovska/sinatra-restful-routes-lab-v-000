2b5521a6166bbb617ed7a974b7e603e3ecaf12c3
Petya Djanovska <petya.djanovska@gmail.com>
2018-06-02 21:01:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[cook_time])
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
   end
 
 end
